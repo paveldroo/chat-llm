f70d9319df2396c72061daf91649ef0d0e782b85
Pavel Druzhinin <paveldroo@gmail.com>
2026-08-12 11:26:09 +0300

    lint clear, review fixes

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 use std::{
     env::{self},
+    error::Error,
     process::ExitCode,
 };
 
 fn main() -> ExitCode {
-    let args: Vec<String> = env::args().collect();
-    let Ok(_) = env::var("LLM_API_KEY") else {
-        eprintln!("chat-llm: you should specify LLM_API_KEY env");
-        return ExitCode::FAILURE;
-    };
-    match args.get(1) {
-        Some(text) => println!("{}", text),
-        None => {
-            eprintln!("chat-llm: no text specified");
-            return ExitCode::FAILURE;
+    match run() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(err) => {
+            eprintln!("chat-llm: {err}");
+            ExitCode::FAILURE
         }
     }
+}
 
-    ExitCode::SUCCESS
+fn run() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let Ok(_) = env::var("LLM_API_KEY") else {
+        return Err("you should specify LLM_API_KEY env".into());
+    };
+    match args.get(1) {
+        Some(text) => println!("{text}"),
+        None => return Err("no text specified".into()),
+    }
+
+    Ok(())
 }
